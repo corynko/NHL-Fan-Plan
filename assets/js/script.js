57ea417c743e3fd452ac625c4491f74d8e6863c3
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-05-30 16:31:04 -0400

    updated API source

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
-var sidebarEl = $("#sidebar-id");
+const url =
+  "https://newscatcher.p.rapidapi.com/v1/search_enterprise?q=Health&lang=en&sort_by=relevancy&country=US&topic=health&page=1&media=True";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "fd74d69c46msh75443648a01bff4p118236jsn4843662a2481",
+    "X-RapidAPI-Host": "newscatcher.p.rapidapi.com",
+  },
+};
 
-var requestURL = "https://www.healthcare.gov/:blog.json";
-
-function getApi(requestURL) {
-  fetch(requestURL).then(function (response) {
-    console.log(response);
-    return response.json();
-  });
+try {
+  const response = await fetch(url, options);
+  const result = await response.text();
+  console.log(result);
+} catch (error) {
+  console.error(error);
 }
-
-getApi(requestURL);
