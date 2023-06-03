408cd0322adef0af1ff6cefeb2fb02f64bac46be
Ethan <131805068+corynko@users.noreply.github.com>
2023-06-03 14:17:48 -0600

    implemented news sidebar with thenews API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,64 @@
+newsCall();
+
+function newsCall() {
+  var requestOptions = {
+    method: "GET",
+  };
+
+  var params = {
+    api_token: "s2MrFnpSyjoXrUViZDIeLTMvwAwsjDrlNnTdBq0N",
+    categories: "sports",
+    search: "nhl",
+    limit: "3",
+    locale: "us",
+    language: "en",
+  };
+
+  var esc = encodeURIComponent;
+  var query = Object.keys(params)
+    .map(function (k) {
+      return esc(k) + "=" + esc(params[k]);
+    })
+    .join("&");
+
+  fetch("https://api.thenewsapi.com/v1/news/all?" + query, requestOptions)
+    .then((response) => response.json())
+    .then(function (result) {
+      console.log(result);
+      var newsParent = $("#news");
+      for (var i = 0; i < 3; i++) {
+        var imageAdd = $("<img>").attr("src", result.data[i].image_url);
+        imageAdd.attr("height", 100);
+        imageAdd.attr("width", 100);
+        newsParent.append(imageAdd);
+
+        var headlineAdd = $("<h4>").addClass("link" + i);
+        var anchorAdd = $("<a>").attr("href", result.data[i].url);
+        anchorAdd.text(result.data[i].title);
+        headlineAdd.append(anchorAdd);
+        headlineAdd.addClass("newsArticle");
+        // console.log(headlineAdd);
+        newsParent.append(headlineAdd);
+
+        var snippetAdd = $("<p>").addClass("py-4");
+        snippetAdd.text(result.data[i].snippet);
+        newsParent.append(snippetAdd);
+      }
+    })
+    .catch((error) => console.log("error", error));
+}
+
+// ============================================================================================================================================================================================
 // API Token for The News API: s2MrFnpSyjoXrUViZDIeLTMvwAwsjDrlNnTdBq0N
 // The News API Documentation: https://www.thenewsapi.com/documentation
 
 //global variables
 var myTeams = [];
-
-// var requestOptions = {
-//   method: "GET",
-// };
-
-// var params = {
-//   api_token: "s2MrFnpSyjoXrUViZDIeLTMvwAwsjDrlNnTdBq0N",
-//   categories: "sports",
-//   search: "nhl",
-//   limit: "3",
-//   locale: "us",
-//   language: "en",
-// };
-
-// var esc = encodeURIComponent;
-// var query = Object.keys(params)
-//   .map(function (k) {
-//     return esc(k) + "=" + esc(params[k]);
-//   })
-//   .join("&");
-
-// fetch("https://api.thenewsapi.com/v1/news/all?" + query, requestOptions)
-//   .then((response) => response.text())
-//   .then((result) => console.log(result))
-//   .catch((error) => console.log("error", error));
-
-// ============================================================================================================================================================================================
 var checkData = window.localStorage.getItem("My-Teams");
-console.log(checkData);
+// console.log(checkData);
 
 if (checkData == null) {
-  console.log("help!");
+  console.log("New User");
 } else {
   myTeams = JSON.parse(window.localStorage.getItem("My-Teams"));
   handlePageLoad();
@@ -60,7 +84,6 @@ function handlePageLoad() {
       .then(function (result) {
         // console.log(result);
         var trEl = $("<tr>").attr("id", savedTeams[i]);
-
         var thRow = $("<th>").attr("scope", "row");
         var tdCity = $("<td>").attr("id", "city" + i);
         var tdRecord = $("<td>").attr("id", "record" + i);
@@ -87,13 +110,14 @@ function handlePageLoad() {
   }
 }
 
+//event listener on submit button
 var submitEl = $("#newTeamSubmit");
 
 submitEl.on("click", function (e) {
   e.preventDefault();
 
   var userCity = $('input[name="formCity"]').val();
-  var userTeam = $('input[name="formTeam"]').val();
+  // var userTeam = $('input[name="formTeam"]').val();
 
   window.localStorage.setItem("City", userCity);
   // window.localStorage.setItem("Name", userTeam);
@@ -102,8 +126,21 @@ submitEl.on("click", function (e) {
 
   // Resets input field
   $('input[name="formCity"]').val("");
-  $('input[name="formTeam"]').val("");
+  // $('input[name="formTeam"]').val("");
 });
+
+// allTeamsEl.on("click", function () {
+// for (var i = 0; i < )
+
+//   window.localStorage.setItem("City", userCity);
+//   // window.localStorage.setItem("Name", userTeam);
+
+//   getTeamID();
+
+//   // Resets input field
+//   $('input[name="formCity"]').val("");
+//   // $('input[name="formTeam"]').val("");
+// });
 
 // NHL Stats API Documentation: https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md
 // Can pull an INSANE amount of information from this API. Teams, rosters, player stats, schedules, scores, all the way from the beginning of the league to present
@@ -136,15 +173,6 @@ function getTeamID() {
 //adds team information to main chart
 function renderMyTeams() {
   var teamID = window.localStorage.getItem("TeamID");
-  // var currentData = JSON.parse(window.localStorage.getItem("My-Teams"));
-
-  // if (currentData != null) {
-  //   for (var i = 0; i < currentData.length; i++) {
-  //     myTeams.push(currentData[i]);
-  //   }
-  //   console.log(myTeams);
-  //   // myTeams.push(currentData);
-  // }
 
   myTeams.push(teamID);
   window.localStorage.setItem("My-Teams", JSON.stringify(myTeams));
